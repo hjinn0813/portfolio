4fbb3e6a26e7a254fe2ce040782321b529b97efc
hjinn0813 <hjc3790@gmail.com>
2024-08-06 15:06:19 +0900

    feat: add comp

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import About from './components/About/About';
 import Skill from './components/Skill/Skill';
+import Education from './components/Education/Education';
 import Project from './components/Project/Project';
 import Contact from './components/Contact/Contact';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header />
-      <Routes>
-        <Route path="/" element={<About />} />
-        <Route path="skill" element={<Skill />} />
-        <Route path="project" element={<Project />} />
-        <Route path="contact" element={<Contact />} />
-      </Routes>
+      <main className="container">
+        <About />
+        <Skill />
+        <Education />
+        <Project />
+        <Contact />
+      </main>
       <Footer />
     </div>
   );
