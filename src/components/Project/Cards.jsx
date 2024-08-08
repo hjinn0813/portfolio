71605f71eee4bd1796e645f77a1d53b2d9b215dd
hjinn0813 <hjc3790@gmail.com>
2024-08-08 15:35:11 +0900

    feat: 페이지 기본구조 설정

--- src/components/Project/Cards.jsx
+++ src/components/Project/Cards.jsx
@@ -2,6 +2,7 @@
 // json 불러와서 돌리기
 
 import React from 'react';
+import '../../styles/Project/Cards.scss';
 
 export default function Cards() {
   return <div>Cards</div>;
--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,6 +1,7 @@
 // 전체 프로젝트 확인 영역
 
 import React from 'react';
+import '../../styles/Project/Project.scss';
 
 export default function Project() {
   return <div>Project</div>;
