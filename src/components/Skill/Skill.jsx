0590c37f88214a694d40805df9d96d9ffdf6d2a3
hjinn0813 <hjc3790@gmail.com>
2024-08-09 15:22:53 +0900

    feat: skill 페이지 최종 디자인

--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -2,37 +2,90 @@
 
 import React from 'react';
 import NextBtn from '../NextBtn';
+import Frontend from './Frontend.json';
+import Library from './Library.json';
+import Control from './Control.json';
+import Develop from './Develop.json';
+import Others from './Others.json';
 import '../../styles/Skill/Skill.scss';
 
 export default function Skill() {
+  const getBadge = (name) => {
+    try {
+      return require(`../../assets/badges/${name}`);
+    } catch (error) {
+      console.error('Image not found:', error);
+      return null; // 이미지가 없을 경우 null 반환
+    }
+  };
+
   return (
     <div className="skill-inner">
       <div className="skill-title">Skills</div>
       <div className="skill-content">
-        <div className="sk-item">
+        <div className="sk-item FE-badge">
           <div className="sk-item-title">Frontend</div>
           <div className="sk-item-value">
-            html css js ts jquery react redux sass nodejs
+            {Frontend.map((badge, index) => (
+              <img
+                key={index}
+                src={getBadge(badge.path)}
+                alt={badge.name}
+                className="badge"
+              />
+            ))}
           </div>
         </div>
-        <div className="sk-item">
+        <div className="sk-item library-badge">
           <div className="sk-item-title">Framework & Library</div>
-          <div className="sk-item-value">bootstrap gsap swiper</div>
+          <div className="sk-item-value">
+            {Library.map((badge, index) => (
+              <img
+                key={index}
+                src={getBadge(badge.path)}
+                alt={badge.name}
+                className="badge"
+              />
+            ))}
+          </div>
         </div>
-        <div className="sk-item">
+        <div className="sk-item git-badge">
           <div className="sk-item-title">Version Control</div>
-          <div className="sk-item-value">git github</div>
+          <div className="sk-item-value">
+            {Control.map((badge, index) => (
+              <img
+                key={index}
+                src={getBadge(badge.path)}
+                alt={badge.name}
+                className="badge"
+              />
+            ))}
+          </div>
         </div>
-        <div className="sk-item">
+        <div className="sk-item develop-badge">
           <div className="sk-item-title">Development & Deploy Tool</div>
           <div className="sk-item-value">
-            vscode eslint prettier netlify vercel
+            {Develop.map((badge, index) => (
+              <img
+                key={index}
+                src={getBadge(badge.path)}
+                alt={badge.name}
+                className="badge"
+              />
+            ))}
           </div>
         </div>
-        <div className="sk-item">
+        <div className="sk-item other-badges">
           <div className="sk-item-title">Others</div>
           <div className="sk-item-value">
-            figma ps slack discord zoom notion
+            {Others.map((badge, index) => (
+              <img
+                key={index}
+                src={getBadge(badge.path)}
+                alt={badge.name}
+                className="badge"
+              />
+            ))}
           </div>
         </div>
         <div className="sk-item">
