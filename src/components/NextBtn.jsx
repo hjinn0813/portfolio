404e452273707ed5174357d808b30693cdca0cf7
hjinn0813 <hjc3790@gmail.com>
2024-08-09 16:59:29 +0900

    fix: 스크롤 이동 기능 수정중

--- src/components/NextBtn.jsx
+++ src/components/NextBtn.jsx
@@ -1,17 +1,19 @@
 // 다음 페이지 이동 버튼
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/NextBtn.scss';
 
 export default function NextBtn({ to, text }) {
-  useEffect(() => {
-    window.scrollTo(0, 0); // 페이지 이동 시 최상단으로
-  }, []);
+  const scrollToTop = () => {
+    if (window.scrollY > 0) {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  };
 
   return (
     <div className="move-btn">
-      <Link to={to} className="move-link">
+      <Link to={to} className="move-link" onClick={scrollToTop}>
         {text}
       </Link>
     </div>
