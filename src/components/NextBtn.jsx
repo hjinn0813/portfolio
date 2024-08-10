166b7c0ae8fe0c8fdfd4e3f5c6f4039bfc0951e4
hjinn0813 <hjc3790@gmail.com>
2024-08-10 10:27:51 +0900

    feat: add arrow effect

--- src/components/NextBtn.jsx
+++ src/components/NextBtn.jsx
@@ -12,10 +12,72 @@ export default function NextBtn({ to, text }) {
   };
 
   return (
-    <div className="move-btn">
-      <Link to={to} className="move-link" onClick={scrollToTop}>
+    <div className="move-btn" style={styles.moveBtn}>
+      <Link
+        to={to}
+        style={styles.moveLink}
+        className="move-link"
+        onClick={scrollToTop}
+      >
         {text}
+        <span style={styles.arrowContainer}>
+          <span style={{ ...styles.arrow, ...styles.firstArrow }}></span>
+          <span style={{ ...styles.arrow, ...styles.secondArrow }}></span>
+        </span>
       </Link>
     </div>
   );
 }
+
+// arrow style
+const styles = {
+  moveBtn: {
+    padding: '10px 20px',
+    borderRadius: '5px',
+    textAlign: 'center',
+    cursor: 'pointer',
+  },
+  moveLink: {
+    display: 'flex',
+    alignItems: 'center',
+    textDecoration: 'none',
+    color: 'inherit',
+  },
+  arrowContainer: {
+    display: 'flex',
+    marginLeft: '16px',
+  },
+  arrow: {
+    width: 0,
+    height: 0,
+    borderTop: '10px solid transparent',
+    borderBottom: '10px solid transparent',
+    borderLeft: '15px solid gray',
+    marginLeft: '-5px',
+    animation: 'move 1s ease-in-out infinite',
+  },
+  firstArrow: {
+    animationDelay: '0.2s',
+  },
+  secondArrow: {
+    animationDelay: '0.7s',
+  },
+};
+
+// @keyframes
+const styleSheet = document.styleSheets[0];
+
+const keyframes = `
+  @keyframes move {
+    0% {
+      transform: translateX(0);
+      opacity: 1;
+    }
+    100% {
+      transform: translateX(10px);
+      opacity: 0;
+    }
+  }
+`;
+
+styleSheet.insertRule(keyframes, styleSheet.cssRules.length);
